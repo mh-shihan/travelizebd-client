dc0030aaf0afe6ea0774d2c7344db39e0c5bcf74
MHShihan <mahmodul.no.1@gmail.com>
2024-01-11 21:28:35 +0600

    Implemented google signIn in the AuthProvider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,15 +1,18 @@
 import { createContext, useEffect, useState } from "react";
 import {
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
@@ -25,6 +28,11 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
   const updateUser = (name, photo) => {
     return updateProfile(auth.currentUser, {
       displayName: name,
@@ -32,12 +40,20 @@ const AuthProvider = ({ children }) => {
     });
   };
 
-  const authInfo = { createUser, signIn, updateUser, user, loading };
+  const authInfo = {
+    createUser,
+    signIn,
+    googleSignIn,
+    updateUser,
+    user,
+    loading,
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("current user", currentUser);
+      setLoading(false);
     });
     return () => {
       return unsubscribe();
