bde390b1a8638752888c9118344cb63129e90a1a
MHShihan <mahmodul.no.1@gmail.com>
2024-01-11 15:05:42 +0600

    Implemented authProvider for creating user and signIn

--- src/pages/home/navbar/Navbar.jsx
+++ src/pages/home/navbar/Navbar.jsx
@@ -50,9 +50,11 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="navbar-end">
-            <a className="btn btn-sm md:btn bg-yellow md:bg-yellow text-base-100 md:text-base-100">
-              Register
-            </a>
+            <Link to="signUp">
+              <button className="btn btn-sm md:btn bg-yellow md:bg-yellow text-base-100 md:text-base-100">
+                Register
+              </button>
+            </Link>
           </div>
         </div>
       </div>
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { createUser, signIn, user, loading };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
