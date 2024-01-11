8f3a70bd22f3a585d234ef4e4dc3f195aeedc2ca
MHShihan <mahmodul.no.1@gmail.com>
2024-01-11 20:48:45 +0600

    Completed register page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,73 +1,36 @@
-import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
-import { useState } from "react";
-import Swal from "sweetalert2";
-import signUpAnimation from "../../assets/animation/signup-animation.json";
 import Lottie from "lottie-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import loginAnimation from "../../assets/animation/login-animation.json";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
-const SignUp = () => {
+const Login = () => {
   const [errorMessage, setErrorMessage] = useState("");
-  const { createUser, updateUser } = useAuth();
-  const navigate = useNavigate();
+  const { signIn } = useAuth();
 
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
-    const name = data?.name;
     const email = data?.email;
     const password = data?.password;
-    const photo = data?.photo;
-    console.log(name, email, password);
 
-    createUser(email, password)
-      .then((res) => {
-        console.log(res.user);
-
-        if (res?.user) {
-          updateUser(name, photo).then(() => {});
-          Swal.fire({
-            icon: "success",
-            title: "Registration Successful",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-          reset();
-          //   navigate("/login");
-        }
-      })
-      .catch((error) => {
-        const err = error.message;
-        setErrorMessage(err);
-      });
+    console.log(email, password);
   };
 
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content w-full flex-col lg:flex-row ">
         <div className="text-center w-1/2 ">
-          <h1 className="text-5xl font-bold mb-6">Register now!</h1>
-
           <Lottie
-            className="h-[420px]"
-            animationData={signUpAnimation}
+            className="h-[400px]"
+            animationData={loginAnimation}
             loop={false}
           ></Lottie>
         </div>
+
         <div className="card  w-full max-w-sm shadow-2xl bg-base-100">
+          <h1 className="text-4xl font-bold mt-6 text-center">Login now!</h1>
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                name="name"
-                {...register("name")}
-                placeholder="name"
-                className="input input-bordered"
-                required
-              />
-            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -81,18 +44,7 @@ const SignUp = () => {
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Photo URL</span>
-              </label>
-              <input
-                type="photo"
-                name="photo"
-                {...register("photo")}
-                placeholder="photo url"
-                className="input input-bordered"
-              />
-            </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
@@ -107,16 +59,14 @@ const SignUp = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-outline bg-slate-800 text-base-100">
-                Login
-              </button>
+              <button className="btn btn-outline  ">Login</button>
             </div>
             {errorMessage && <p className="text-red-600">{errorMessage}</p>}
             <Link to="/login">
               <p className="block mt-4 font-sans text-base antialiased font-normal leading-relaxed text-center text-gray-700">
                 Already have an account?
                 <a href="#" className="font-medium text-gray-900">
-                  Register
+                  Login
                 </a>
               </p>
             </Link>
@@ -127,4 +77,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Login;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
 import ErrorPage from "../pages/error/ErrorPage";
 import SignUp from "../pages/signUp/SignUp";
+import Login from "../pages/login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "signUp",
         element: <SignUp></SignUp>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
       },
     ],
   },
