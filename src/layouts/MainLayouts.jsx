1645f1fa48cd41780b62a12cc5c3853bd5afd9f4
MHShihan <mahmodul.no.1@gmail.com>
2024-01-11 10:38:10 +0600

    first commit

---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,13 @@
+import Home from "../pages/home/home/Home";
+
+const MainLayouts = () => {
+  return (
+    <div>
+      <header>
+        <Home></Home>
+      </header>
+    </div>
+  );
+};
+
+export default MainLayouts;
